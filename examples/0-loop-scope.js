cd6bdf3651bb708812b378068b77e63532dbfa0a
richardcrng@gmail.com <richardcrng@gmail.com>
2021-06-03 07:01:41 +0100

    Simple first example of loop scoping

--- examples/0-loop-scope.js
+++ examples/0-loop-scope.js
@@ -3,21 +3,17 @@
 let myString = 'testing out variable scope';
 let words = myString.split(' ');
 
+console.log('Entering my loop:');
+
 for (let word of words) {
   let capitalisedWord = word.toUpperCase();
   console.log('I shout:', capitalisedWord);
 }
 
-console.log('-------'); // separator to make output clearer
+console.log('Loop exited!')
 
-console.log("can I access globally-scoped variable myString here? yes!", myString);
-console.log("can I access globally-scoped variable words here? yes!", words);
-
-console.log('-------'); // separator to make output clearer
-
-// below line will error - comment out to fix
-console.log("can I access loop-scoped variables word here? no...", word);
-console.log("can I access loop-scoped variables capitalisedWord here? no...", capitalisedWord);
-
+// below will cause errors - comment out to fix
+console.log("outside a loop, can I access word?", word);
+console.log("outside a loop, can I access capitalisedWord?", capitalisedWord);
 
 console.log("Reached the end of my programme!");
