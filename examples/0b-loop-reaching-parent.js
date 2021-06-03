6673d1be0083c8bdea564408e09802f429b6b9cb
Richard Ng <richardcrng@gmail.com>
2021-06-03 07:17:10 +0100

    Group together Ex0 examples

--- examples/0b-loop-reaching-parent.js
+++ examples/0b-loop-reaching-parent.js
@@ -5,9 +5,13 @@ console.log("Start of programme");
 let fruitArr = ['apple', 'grape', 'pear'];
 let stringSeparator = '-- PLUS --';
 
+console.log("Entering my loop:");
+
 for (let fruit of fruitArr) {
-  console.log('In loop scope:', fruit);
-  console.log('Also in loop scope:', stringSeparator);
+  console.log(fruit);
+  console.log(stringSeparator);
 }
+
+console.log("Exited my loop!")
 
 console.log("Reached the end of my programme!");
