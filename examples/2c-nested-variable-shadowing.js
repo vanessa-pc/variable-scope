ae0798dc6816524ce3a0729a8c47cf82787e2fe3
Richard Ng <richardcrng@gmail.com>
2021-06-03 09:40:10 +0100

    Add shadowing example

--- examples/2c-nested-variable-shadowing.js
+++ examples/2c-nested-variable-shadowing.js
@@ -1,13 +1,27 @@
 let scope = "global";
+let inGlobal = "I exist in global scope!"
 
 function printScopeExample() {
   let scope = "function inside global";
+  let inFunction = "I exist in the function's scope!"
+
   for (let i = 1; i <= 2; i++) {
     let scope = "loop inside function inside global";
+    let inLoopIteration = "I exist in the loop iteration's scope!";
     console.log("my scope is", scope);
+    console.log(inLoopIteration);
+    console.log(inFunction);
+    console.log(inGlobal);
   }
+
   console.log("my scope is", scope);
+  console.log(inLoopIteration);
+  console.log(inFunction);
+  console.log(inGlobal);
 }
 
 printScopeExample();
 console.log("my scope is", scope);
+console.log(inLoopIteration);
+console.log(inFunction);
+console.log(inGlobal);
