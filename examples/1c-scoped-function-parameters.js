2d4a25c72daa46dada5348d0551f2bd9e70816ce
Vasiliki Pasalidi-Chantzi <van3pasa@hotmail.com>
2021-07-29 16:40:51 +0300

    Fix code for 1c-scoped-function-parameters

--- examples/1c-scoped-function-parameters.js
+++ examples/1c-scoped-function-parameters.js
@@ -2,12 +2,10 @@
 
 function printGreeting(name) {
   console.log(`Hello, ${name}!`);
+  console.log("The name chosen was", name);
 }
 
 printGreeting("Richard");
 printGreeting("Natalya");
-
-// below line will error
-console.log("The name chosen was", name);
 
 console.log("Programme complete!")
