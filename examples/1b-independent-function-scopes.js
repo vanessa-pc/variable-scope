5c884bf62ed5aec5734b5852708bcd40a63e5b18
Vasiliki Pasalidi-Chantzi <van3pasa@hotmail.com>
2021-07-29 16:39:29 +0300

    Fix code for 1b-independent-function-scopes

--- examples/1b-independent-function-scopes.js
+++ examples/1b-independent-function-scopes.js
@@ -7,9 +7,7 @@ function printWithPrefix() {
 
   const prefix = "quick";
   console.log("printing with prefix:", `${prefix}${commonOuterScope}`);
-
-  // below line will error, comment out to fix
-  console.log("trying to access suffix:", `${commonOuterScope}${suffix}`);
+  console.log('trying to access prefix:', `${prefix}${commonOuterScope}`)
 }
 
 
@@ -18,9 +16,7 @@ function printWithSuffix() {
 
   const suffix = "castle";
   console.log('printing with suffix:', `${commonOuterScope}${suffix}`);
-
-  // below line will error, comment out to fix
-  console.log('trying to access prefix:', `${prefix}${commonOuterScope}`)
+  console.log("trying to access suffix:", `${commonOuterScope}${suffix}`);
 }
 
 printWithPrefix();
